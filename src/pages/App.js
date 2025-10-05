213f4d4db3720478a62c8889e5a38c016e9dc6b6
jocile <jocile@gmail.com>
2025-10-05 15:17:16 -0300

    Add botao buscar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,23 @@
 
+import { useState } from 'react';
 import gitLogo from '../assets/github-mark-white.svg'
 import Input from '../components/Input'
+import Button from '../components/Button'
 import ItemRepo from '../components/ItemRepo'
 
 import { Container } from './styles';
+
 function App() {
+
+  const [repos, setRepos] = useState([]);
+
+
+
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt='GitHub Logo' />
       <Input />
+      <Button />
       <ItemRepo />    
     </Container>
   );
